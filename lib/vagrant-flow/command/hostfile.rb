7ff4719df93b3b49ff9ecb4083007cb99f8f3b10
Lih Chen <c-lchen@kixeye.com>
2014-03-28 19:49:46 -0700

    FIXED: writing duplicate host entries when running the host option multiple times.

--- lib/vagrant-flow/command/hostfile.rb
+++ lib/vagrant-flow/command/hostfile.rb
@@ -6,14 +6,16 @@ module VagrantPlugins
   module CommandVagrantFlow
     module Command
       class HostFile < Vagrant.plugin("2", :command)
-        
+        @@begin_tag_line = "##BEGIN: ADDED BY NEVERWINTERDP'S VAGRANT-FLOW HOSTFILE COMMAND"
+        @@end_tag_line = "##END: ADDED BY NEVERWINTERDP'S VAGRANT-FLOW HOSTFILE COMMAND"
+
         # Builtin from Command class
         # Must override to provide a description
         def self.synopsis
           "Sets /etc/hosts so that all your machines can communicate with each other"
         end
-        
-        
+
+
         # Builtin from Command class
         # Must override to provide core functionality
         def execute
@@ -23,27 +25,27 @@ module VagrantPlugins
           options[:provision_ignore_sentinel] = false
           options[:nowrite] = false
           options[:quiet] = false
-          
-          #Parse option, look up OptionParser documentation 
+
+          #Parse option, look up OptionParser documentation
           opts = OptionParser.new do |o|
             # o.banner = "Usage: vagrant ansible-inventory [vm-name] [options] [-h]"
             o.banner = "A NeverWinterDP technology from the Department of Badass.\n\n"+
                         "Usage: vagrant flow hostfile [-hnkq]\n"+
                         "Edits all your VMs /etc/hosts file to be able to find all the machines on your private network"
             o.separator ""
-           
+
             o.on("-q", "--quiet", "(Optional) Suppress output to STDOUT and STDERR") do |f|
               options[:quiet] = true
             end
-            
+
             o.on("-n", "--no-write-hosts", "(Optional) Don't actually write a new hosts file to the guest machine") do |f|
               options[:nowrite] = true
             end
           end
           argv = parse_options(opts)
           return if !argv
-          
-          
+
+
           hostinfo=[]
           #Go through config
           #Map hostnames to IP's
@@ -58,11 +60,11 @@ module VagrantPlugins
                         :hostname=>hostname,
                         :ip=>net[:ip]
                     })
-                end                
+                end
               }
             }
           end
-          
+
           #Print out Hostname and IP
           if not options[:quiet]
             puts "HOSTS FILE INFO:"
@@ -70,21 +72,21 @@ module VagrantPlugins
               puts x[:hostname]+": "+x[:ip]
             }
           end
-          
+
           #What we're going to add to the end of the guest's hostfile
           #A combination of IP's and hostnames
-          toconcatenate="\n"+"##ADDED BY NEVERWINTERDP'S VAGRANT-FLOW HOSTFILE COMMAND"+"\n"
+          toconcatenate="\n"+@@begin_tag_line+"\n"
           hostinfo.each {|x|
-            toconcatenate+=x[:ip]+"   "+x[:hostname]+"\n"
+            toconcatenate += x[:ip]+"   "+x[:hostname]+"\n"
           }
-          
-          
+          toconcatenate += @@end_tag_line+"\n"
+
           #Read in guest's current hosts file
           #Append our new entries to it
           #Write it out onto the guest's hosts file
           with_target_vms(argv, :provider => options[:provider]) do |machine|
             return unless machine.communicate.ready?
-            
+
             #This block of code was shamelessly stolen from
             #https://github.com/smdahlen/vagrant-hostmanager/blob/master/lib/vagrant-hostmanager/hosts_file.rb
             #Why reinvent the wheel?
@@ -95,35 +97,51 @@ module VagrantPlugins
             elsif (machine.communicate.test("test -d $Env:SystemRoot"))
               realhostfile = "#{ENV['WINDIR']}\\System32\\drivers\\etc\\hosts"
               move_cmd = 'mv -force'
-            else 
+            else
               realhostfile = '/etc/hosts'
               move_cmd = 'mv'
             end
-            
+
             #Back to my code
             #Grab Vagrant's temp file location
+            guesthostfilepath_original = @env.tmp_path.join("hosts."+machine.config.vm.hostname+".og")
             guesthostfilepath = @env.tmp_path.join("hosts."+machine.config.vm.hostname)
+
             #Download guest's hosts file
-            machine.communicate.download(realhostfile, guesthostfilepath)
-            
+            machine.communicate.download(realhostfile, guesthostfilepath_original)
+
+            #Skip everything between ##BEGIN and ##END
+            guesthostfile = File.open(guesthostfilepath, "w")
+            skip = false
+            File.open(guesthostfilepath_original, "r") do |infile|
+              while (line = infile.gets)
+                if line.strip() == @@begin_tag_line
+                  skip = true
+                elsif line.strip() == @@end_tag_line
+                  skip = false
+                else
+                  guesthostfile.write(line) if not skip
+                end
+              end
+            end
+
             #Append our hostname/IP info to the end of the hosts file
-            guesthostfile = File.open(guesthostfilepath,"a")
             guesthostfile.write(toconcatenate)
             guesthostfile.close()
-            
-            if not options[:nowrite]  
+
+            if not options[:nowrite]
               #Upload updated hosts file to guest to temp location
               machine.communicate.upload(guesthostfile.path, '/tmp/hosts')
               #Move temp file over on top of hosts file
-              machine.communicate.sudo(move_cmd+" /tmp/hosts "+realhostfile)  
+              machine.communicate.sudo(move_cmd+" /tmp/hosts "+realhostfile)
             end
           end
-          
-          
-            
+
+
+
         end
-        
-      end       
-    end    
+
+      end
+    end
   end
 end
