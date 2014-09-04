afebd287a9f477e7bae316593f40288bac843da0
KingOfPoptart <richard.c.duarte@gmail.com>
2014-09-04 17:05:10 +0200

    Adding file to install DO key

---
+++ lib/vagrant-flow/command/installDOKey.rb
@@ -0,0 +1,80 @@
+require "vagrant"
+require 'optparse'
+require 'tempfile'
+require 'yaml'
+
+#Require my library for talking to digitalocean
+require File.expand_path(File.dirname(__FILE__) ) +"/digitalocean_api"
+
+module VagrantPlugins
+  module CommandVagrantFlow
+    module Command
+      class InstallDOKey < Vagrant.plugin("2", :command)
+        
+        # Builtin from Command class
+        # Must override to provide a description
+        def self.synopsis
+          "Installs your digital ocean key to ~/.vagrant-flow"
+        end
+
+        def commandThread(machine,command, quiet)
+          begin
+            machine.communicate.execute(command)
+            if !quiet
+              puts "Command was ran successfully on: "+machine.config.vm.hostname
+            end
+          rescue
+            if !quiet
+              puts "Command FAILED on: "+machine.config.vm.hostname
+              @error_message="#{$!}"
+              puts @error_message
+            end
+          ensure
+            if !quiet
+              puts "----"
+            end
+          end
+        end
+        
+        # Builtin from Command class
+        # Must override to provide core functionality
+        def execute
+          options = {}
+          options[:destroy_on_error] = true
+          options[:parallel] = false
+          options[:provision_ignore_sentinel] = false
+          options[:quiet] = false
+          options[:token] = nil
+
+          opts = OptionParser.new do |o|
+            o.banner = "A NeverWinterDP technology from the Department of Badass.\n\n"+
+                        "Usage: vagrant flow multicommand [-qf] -c COMMAND\n"+
+                        "Installs your digital ocean key for easy vagrant-flow configuration"
+            o.separator ""
+
+            o.on("-q", "--quiet", "(Optional) Suppress output to STDOUT and STDERR") do |f|
+              options[:quiet] = true
+            end
+            
+            
+            o.on("-t", "--token TOKEN", "(REQUIRED) The token to install") do |f|
+              options[:token] = f
+            end
+          end
+          argv = parse_options(opts)
+          return if !argv
+          raise OptionParser::MissingArgument if options[:token].nil?
+          x = {
+            :digitalOceanToken=>options[:token],
+            }
+          begin
+            File.open('~/.vagrant-flow', 'w') { |file| f.write x.to_yaml  }
+          rescue
+            $stderr.print "Writing to ~/.vagrant-flow failed: " + $!
+          end
+          
+        end
+      end
+    end
+  end
+end
